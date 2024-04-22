e8ac4e62880a2ca62986256ca9e1eabbe9afdbed
Michele <p_miki89@hotmail.it>
2024-04-22 17:41:10 +0200

    Initial commit

---
+++ DaYukina/DaYukina/src/main/java/com/yukina/spa/DaYukina/DaYukinaApplication.java
@@ -0,0 +1,13 @@
+package com.yukina.spa.DaYukina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaYukinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaYukinaApplication.class, args);
+	}
+
+}
---
+++ DaYukina/DaYukina/src/test/java/com/yukina/spa/DaYukina/DaYukinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukina.spa.DaYukina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaYukinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
